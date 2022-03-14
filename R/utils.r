30ee85e7445db1b8909cbbbebb2beb3bc2223e98
HatsuneCode <hatsunecode@gmail.com>
2022-03-14 21:48:56 +0800

    lhsong

--- R/utils.r
+++ R/utils.r
@@ -63,3 +63,44 @@ timer = function() as.character(Sys.time())
 #' df_chain(list('V', 'D', 'J', 'C', 'CDR3nt', 'CDR3aa', '60', 'id1', '98', '1'))
 #'
 df_chain = function(chain) setNames(data.frame(t(unlist(chain))), chainName)
+
+#' cbind two data.frame
+#' 
+#' cbind two data.frame by filling missing rows from each other based on their row.names.
+#'
+#' @param F1 data.frame.
+#' @param F2 data.frame.
+#' @param fill character/numeric. Default 0
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' F1 = data.frame(A = seq(10), B = seq(10), row.names = seq(10))
+#' F2 = data.frame(C = seq(5),  D = seq(5),  row.names = 3:7)
+#' cbinds(F1, F2, fill = 'none')
+#' 
+cbinds = function(F1, F2, fill = 0) {
+  if(any(dim(F1) == 0)) return(F2)
+  if(any(dim(F2) == 0)) return(F1)
+  rowall = c(rownames(F1), rownames(F2))
+  dF1 = setdiff(rowall, rownames(F1))
+  dF2 = setdiff(rowall, rownames(F2))
+  if(length(dF1)){
+    SF1r           = matrix(fill, nrow = length(setdiff(rowall, rownames(F1))), ncol = ncol(F1))
+    rownames(SF1r) = dF1
+    colnames(SF1r) = colnames(F1)
+    F1             = rbind(F1, SF1r)
+    rm(SF1r)
+  }
+  if(length(dF2)){
+    SF2r           = matrix(fill, nrow = length(setdiff(rowall, rownames(F2))), ncol = ncol(F2))
+    rownames(SF2r) = dF2
+    colnames(SF2r) = colnames(F2)
+    F2             = rbind(F2, SF2r)
+    rm(SF2r)
+  }
+  F2 = F2[rownames(F1), , drop = F]
+  cbind(F1, F2)
+}
+
