6a2c21f75986ab130c345408888d4d1a1d6c9dbc
HatsuneCode <hatsunecode@gmail.com>
2022-03-15 22:22:32 +0800

    lhsong

--- R/download.r
+++ R/download.r
@@ -3,61 +3,70 @@ NULL
 
 #' Download files retryably
 #'
-#' @param x character. URLs.
-#' @param name character. file names.
-#' @param sleep numeric. retry interval.
-#' @param outdir character. output directory. 
+#' @param URLs character/list. URLs to be downloaded.
+#' @param names character/list. file names. Default \code{seq(URLs)}
+#' @param method character. Method to be used for downloading files, equal to \code{download.file}. Default 'libcurl'
+#' @param sleep numeric. retry interval (second). Default 2
+#' @param outdir character. output directory.
+#' @param verbose logical. Default TRUE
 #'
 #' @importFrom utils download.file
 #'
-#' @return downloaded files
+#' @return if success, return TRUE
 #' @export
 #'
 #' @examples
 #' URLs = paste0('http://www.imgt.org//download/', c('V-QUEST/IMGT_V-QUEST_reference_directory',
 #'          'GENE-DB/IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP'))
-#' # Download(URLs, c('vdj_species.html', 'IMGT_download.fa') )
-#' 
-Download = function(x, name = NULL, sleep = NULL, outdir = NULL){
-  
+#' Download(URLs, c('vdj_species.html', 'IMGT_download.fa'))
+#'
+Download = function(URLs, names = NULL, method = NULL, sleep = NULL, outdir = NULL, verbose = TRUE){
+
   # check parameter #
-  x      = as.character(x)
+  URLs   = as.character(unlist(URLs))
+  names  = as.character(unlist(names) %|||% seq(URLs))
+  method = as.character(method %|||% 'libcurl')
   sleep  = as.numeric(sleep) %|||% 2
-  name   = as.character(name %|||% seq(x))
   outdir = as.character(outdir %|||% getwd())
-  
+
   # set index
   log = 'Download.log'
   writeLines('ok', log)
-  
-  for (i in seq(x)) {
+
+  for (i in seq(URLs)) {
+    name = names[i]
+    URL  = URLs[i]
+
     # check file
-    if(!any(list.files(outdir) %in% name[i])) {
-      tryCatch(download.file(x[i], paste0(outdir, '/', name[i]), 'libcurl', TRUE),
-               error = function(e) { 
+    if(!length(list.files(outdir, paste0('^', name, '$')))) {
+
+      # download
+      if(verbose) cat('-->', timer(), i, ': downloading for', name, '<--\n')
+      tryCatch(download.file(URL, paste0(outdir, '/', name), method, TRUE),
+               error = function(e) {
                  writeLines('Erro', log)
-                 warning('--! ', timer(), ' ', i, ': download fail for ', name[i], ' !--') })
-      
+                 warning('--! ', timer(), ' ', i, ': download fail for ', name, ' !--') })
+
       # check index
       Log = readLines(log)
-      if('ok' %in% Log) cat('--> ', timer(), ' ', i, ': download ok for ', name[i], ' <--\n')
-      
+      if('ok' %in% Log & verbose) cat('-->', timer(), i, ': download ok for', name, '<--\n')
+
       # retry
       while ('Erro' %in% Log){
         writeLines('ok', log)
-        tryCatch(download.file(x[i], destfile = paste0(outdir, '/', name[i]), 'libcurl', TRUE),
+        tryCatch(download.file(URL, destfile = paste0(outdir, '/', name), method, TRUE),
                  error = function(e){
                    writeLines('Erro', log)
-                   warning('--! ', timer(), ' ', i, ': download fail for ', name[i], ' !--') })
-        
+                   warning('--! ', timer(), ' ', i, ': download fail for ', name, ' !--') })
+
         # check index
         Log = readLines(log)
-        if('ok' %in% Log) cat('--> ', timer(), ' ', i, ': download ok for ', name[i], ' <--\n')
-        
+        if('ok' %in% Log & verbose) cat('-->', timer(), i, ': download ok for', name, '<--\n')
+
         # sleep
         Sys.sleep(sleep)
       }
-    } else warning('--! ', timer(), ' ', i, ': file already exist for ', name[i], ' !--')
+    } else warning('--! ', timer(), ' ', i, ': file already exist for ', name, ' !--')
   }
+  file.remove(log)
 }
-
--- R/refIMGT.r
+++ R/refIMGT.r
@@ -1,29 +1,37 @@
 #' @include utils.r download.r
 NULL
 
-build_IMGT_reference = function(outdir = NULL) {
-  
+#' Build IMGT database reference
+#'
+#' @param outdir character. Default \code{getwd()}
+#' @param verbose logical. Default TRUE
+#'
+#' @importFrom rvest read_html html_node html_text
+#' @importFrom Biostrings readBStringSet chartr
+#'
+#' @return if success, return TRUE
+#' @export
+#'
+#' @examples
+#' build_IMGT_reference('IMGT_reference')
+#'
+build_IMGT_reference = function(outdir = NULL, verbose = TRUE) {
+
   # check parameter
   outdir = as.character(outdir %|||% getwd())
-  name   = 'IMGT_reference'  
-
-  # start #
-  cat('-->', timer(), '1. Build VDJ reference from IMGT website <--\n')
-  cat('-->', timer(), 'process outdir (make sure you have permission):', outdir, '<--\n')
-  dir.create(name, FALSE); setwd(name)
-  wdir = getwd()
+  if(verbose) cat('-->', timer(), '1. Build VDJ reference from IMGT website in:', outdir, '<--\n')
+  dir.create(outdir, FALSE, TRUE); setwd(outdir)
 
   # catch #
   species_web = 'vdj_species.html'
   species_fa  = 'IMGT_download.fa'
-  cat('-->', timer(), 'catch latest species list from IMGT website <--\n')
   URLs = paste0('http://www.imgt.org//download/', c('V-QUEST/IMGT_V-QUEST_reference_directory',
                   'GENE-DB/IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP'))
-  Download(URLs, c(species_web, species_fa))
+  Download(URLs, c(species_web, species_fa), verbose = verbose)
 
   # process html#
-  species_html = rvest::html_node(rvest::read_html(species_web), 'body section table')
-  species = sub('.', '', sub('/.*', '', grep('/', strsplit(rvest::html_text(species_html), '- ')[[1]], value = TRUE)))
+  species_html = rvest::html_text(rvest::html_node(rvest::read_html(species_web), 'body section table'))
+  species = sub('.', '', sub('/.*', '', grep('/', strsplit(species_html, '- ')[[1]], value = TRUE)))
 
   # read fa #
   fa = Biostrings::readBStringSet(species_fa)
@@ -31,15 +39,16 @@ build_IMGT_reference = function(outdir = NULL) {
 
   # extract by species #
   lapply(sort(unique(species)), function(sp) {
-  
-    fa_sp = fa[sapply(fa_name, function(nm) grepl(sp, gsub(' ', '_', nm[3])))]
+    fa_sp = fa[sapply(fa_name, function(nm) grepl(sp, gsub(' ', '_', nm[3])) & grepl('^IG|^TR', nm[2]) )]
     if(length(fa_sp)) {
-      cat('-->', timer(), 'process fa in:', sp, '<--\n')
-    
-    
-    
+      if(verbose) cat('-->', timer(), 'process fa for:', sp, '<--\n')
+      names(fa_sp) = sapply(strsplit(names(fa_sp), '\\|'), function(nm) nm[2])
+      fa_sp = Biostrings::chartr('acgtn', 'ACGTN', fa_sp)
+      writeXStringSet(fa_sp, paste0('IMGT_', sp, '.fa'))
+      if(verbose) cat('-->', timer(), 'saved fa in:', sp, '<--\n')
     } else warning('--! ', timer(), ' no fa content in: ', sp, ' !--')
-  
   })
-}
 
+  # done
+  TRUE
+}
