5a81a80aac60c841b131c0f2c140788676188738
HatsuneCode <hatsunecode@gmail.com>
2022-07-12 14:53:42 +0800

    lhsong

--- R/plot.r
+++ R/plot.r
@@ -32,3 +32,42 @@ Pie = function(x, y, fill = c('white', '#f8766d'), color = 'grey') {
                    legend.position = 'none')
 }
 
+#' Plot VJab Sankey
+#'
+#' @param VJab character 
+#' @param out  character
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' VJab = c('TRAV13-1~TRAJ49~TRBV20-1~TRBJ2-2', 'TRAV14DV4~TRAJ13~TRBV20-1~TRBJ2-5', 
+#'          'TRAV1-2~TRAJ33~TRBV6-4~TRBJ2-3',   'TRAV5~TRAJ3~TRBV20-1~TRBJ2-3' )
+#' Sankey(VJab)
+#' 
+Sankey = function(VJab, out = NULL) {
+  suppressMessages(library(ggalluvial))
+  df      = setNames(data.frame(do.call(rbind, strsplit(VJab, '~'))), 
+                     c('Va', 'Ja', 'Vb', 'Jb'))
+  color   = unlist(lapply(apply(df, 2, function(i) length(unique(i)) ), function(i) 
+    colorRampPalette(RColorBrewer::brewer.pal(10, 'Spectral'))(i) ))
+  Freq    = data.frame(sort(table(VJab), T))
+  df$Freq = sapply(1:nrow(df), function(i) 
+    Freq$Freq[match(VJab[i], Freq$VJab)] )
+  df      = df[order(-df$Freq), ]
+  p       = ggplot2::ggplot(df, ggplot2::aes(
+    axis1 = factor(Va, unique(Va)), axis2 = factor(Ja, unique(Ja)),
+    axis3 = factor(Vb, unique(Vb)), axis4 = factor(Jb, unique(Jb)) )) +
+    ggalluvial::geom_alluvium(ggplot2::aes(
+      fill = factor(Va, unique(Va))), width = .3, knot.pos = .5, curve_type = 'cubic') +
+    ggplot2::scale_fill_manual(values = rev(
+      colorRampPalette(RColorBrewer::brewer.pal(10, 'Spectral'))(length(unique(df$Va))))) +
+    ggalluvial::geom_stratum(width = .3, fill = color, color = color) +
+    ggplot2::geom_text(stat = 'stratum', 
+                       ggplot2::aes(label = ggplot2::after_stat(stratum)), check_overlap = T) +
+    ggplot2::theme_void() + 
+    ggplot2::theme(legend.position = 'none')
+  if(have(out)) ggplot2::ggsave(as.character(out), p, w = 16, h = 12) 
+  print(p)
+}
+
